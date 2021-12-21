36b7dfe0f146e068b95dfd110735ccafe9b23a18
ThanhNMSE130286 <thanhnmse130286@fpt.edu.vn>
2021-12-21 16:57:30 +0700

    new blank service home

---
+++ js/ajaxService.js
@@ -0,0 +1,12 @@
+/*
+* url lấy tất cả shop loại dịch vụ : [GET] http://localhost:9090/shop?categoryID=8
+* lưu sessionStorage
+* VD trả về
+* data: {data: ...,
+*        message: ...,
+*        status: ...}
+* */
+
+function fillServiceHome() {
+    /*onload body, kiếm theo id điền vào chỗ trống*/
+}
